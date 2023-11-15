da895576ccea5c1e5e6283fe1fb72cf76ad229d3
Thaker13 <149239169+Thaker13@users.noreply.github.com>
2023-11-15 23:35:11 +0000

    Add files via upload

---
+++ DowhileEvenOdd.java
@@ -0,0 +1,22 @@
+package dowhileevenodd;
+
+public class DowhileEvenOdd {
+    public static void main (String[] args){
+
+        int i = 1;
+        int j = 2;
+        do {
+            System.out.println (i + "odd number");
+            i = i+1;
+        }
+
+        while ( i <= 10);
+        do {
+
+            System .out.println (j +"even number");
+            j=j+ 2;
+
+        }
+        while (j <=20);
+    }
+}
